3b0b01d864da833f19845ac2e3f4ede0554b4eb1
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-10-18 13:40:12 -0700

    feat: Implement `softmax`

--- gpt_from_scratch/functional.py
+++ gpt_from_scratch/functional.py
@@ -1,4 +1,19 @@
 
 from . import tensor
-def linear(input: tensor.Tensor, weight: tensor.Tensor, bias: tensor.Tensor=None): 
+from .utils import _enforce_type
+import math
+
+def linear(input: tensor.Tensor, weight: tensor.Tensor, bias: tensor.Tensor=None):
+    _enforce_type(input, tensor.Tensor)
+    _enforce_type(weight, tensor.Tensor)
+    _enforce_type(bias, tensor.Tensor)
     return input @ weight + bias
+
+
+def softmax(input: tensor.Tensor, dim: int=None): 
+    _enforce_type(input, tensor.Tensor)
+    if not dim is None: _enforce_type(dim, int)
+    print(tensor.sum(math.e**input, dim=dim))
+    return math.e**input / tensor.sum(math.e**input, dim=dim)
+
+
--- tests/test_module.py
+++ tests/test_module.py
@@ -2,10 +2,12 @@ import unittest, os, sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
+from gpt_from_scratch.functional import softmax
 from gpt_from_scratch.module import Linear
 from gpt_from_scratch.tensor import tensor
 
 model = Linear(2, 3)
 
 print(model(tensor([[1, 2], [2, 3]])))
+print(softmax(model(tensor([[1, 2], [2, 3]])), dim=0))
 print(model.parameters())
