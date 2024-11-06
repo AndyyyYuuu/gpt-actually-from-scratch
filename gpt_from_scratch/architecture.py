e2a587afafe80fe769ac38ff48f367530367b5a9
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-11-05 18:22:42 -0800

    wip: Create `AttentionHead` and `architecture.py`

---
+++ gpt_from_scratch/architecture.py
@@ -0,0 +1,26 @@
+from . import functional as F
+from . import tensor, module
+from .parameter import Parameter
+from .utils import _enforce_type
+
+
+class AttentionHead(tensor.Module): 
+
+    def __init__(self, head_size: int, n_embed) -> None:
+        super().__init__()
+        self.key = module.Linear(n_embed, head_size, bias=False)
+        self.query = module.Linear(n_embed, head_size, bias=False)
+        self.value = module.Linear(n_embed, head_size, bias=False)
+        self.dropout = module.Dropout(0.2)
+
+    def forward(self, x): 
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        weights = q @ k.transpose(-2, -1) * C**-0.5
+        weights = weights.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        weights = F.softmax(weights, dim=-1)
+        weights = self.dropout(weights)
+        v = self.value(x)
+        out = weights @ v
+        return out
